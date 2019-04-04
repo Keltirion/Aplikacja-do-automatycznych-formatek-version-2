f8022095cc03e05af479b30c4a0cc04317f6ea21
Lech Lonczynski <llonczynski@gmail.com>
2019-04-04 14:20:37 +0200

    Some updates

--- Functions.py
+++ Functions.py
@@ -2,14 +2,15 @@ import datetime, xlrd, sys, traceback, time
 
 data_list = []
 # External files. They must have the same name as given in the code.
-file = 'test2.xlsx'
+file = 'Clients.xlsx'
 file_email = 'email_body.txt'
 
 # File information. Extract from the file first row and the number of rows.
 xl_workbook = xlrd.open_workbook(file)
 xl_sheet = xl_workbook.sheet_by_name('Sheet1')
 xl_rows = xl_sheet.nrows
-
+# Function for clearing the data from unnecessary informations.
+start = 'Do you want to start creating emails? There will be ' + str(xl_rows) + ' emails?'
 # Email data. Preparing the email body from .txt file
 topic = "Questions regarding the delivery of your VELFAC ORDER ({})"
 body = open(file_email, 'r', encoding='UTF-8')
@@ -19,43 +20,52 @@ today = datetime.date.today()
 next_monday = today + datetime.timedelta(days=-today.weekday(), weeks=2)
 prior_friday = next_monday - datetime.timedelta(days=3)
 
-# Function for clearing the data from unnecessary informations.
-def dataprep(max_range, storage):
-    for i in range(1, max_range):
-        xl_row = xl_sheet.row_values(i)
-        del xl_row[1]
-        del xl_row[6:15]
-        storage.append(xl_row)
 
-# Function for email creation.
-def main():
-    # Depending on the user input. Action will be taken.
-    try:
-        if start == 'yes' or 'Yes':
-            # Clearing the data wit function.
-            dataprep(xl_rows, data_list)
-            # Adding the data from the file to email .txt file to subject and recipient.
-            for i in range(1, xl_rows):
-                address = data_list[0][5]
-                topic_formated = topic.format(int(data_list[0][0]))
-                print('Email nr {} was generated. {} Left. (Press CTRL + C to abort)'.format(i, (int(xl_rows) - i)))
-                # Email creation with data extraced from the file. Email class was used here.
-                Emailer(address, topic_formated, contents.format(int(data_list[0][0]),
+class DataPreparation:
+
+    def __init__(self, max_range, storage):
+        self.max_range = max_range
+        self.storage = storage
+
+    def datamixer(self):
+        for i in range(1, self.max_range):
+            xl_row = xl_sheet.row_values(i)
+            del xl_row[1]
+            del xl_row[6:15]
+            self.storage.append(xl_row)
+
+
+Emailer class with creation method.
+class Emailer:
+    def __init__(self, recipient, subject, body,):
+        self.recipient = recipient
+        self.subject = subject
+        self.body = body
+    # Creates an email within outlook. Three parametrs mus be given.
+    # Subject, recipient and body.
+    def create(self):
+        outlook = win32.Dispatch('outlook.application')
+        email = outlook.CreateItem(0)
+        email.To = self.recipient
+        email.Subject = self.subject
+        email.htmlBody = self.body
+        email.Display(True)
+
+
+class EmailBuilder:
+    def __init__(self, range):
+        self.range = range
+
+    def create(self):
+        for i in range(1, xl_rows):
+            address = data_list[0][5]
+            topic_formated = topic_format(int(data_list[0][0]))
+            # Email creation with data extraced from the file. Emailer class was used here.
+            Emailer(address, topic_formated, contents.format(int(data_list[0][0]),
                                                                  next_monday,
                                                                  data_list[0][1],
                                                                  data_list[0][2],
                                                                  data_list[0][4],
                                                                  data_list[0][3],
                                                                  prior_friday)).create()
-                del data_list[0]
-        # User selected "No" program closes.
-        elif start == 'no' or 'No':
-            quit()
-    # User presed Ctr+c for aborting the script.
-    except KeyboardInterrupt:
-        print('Script aborted by the user - closing in 10 sec')
-        time.sleep(10)
-    # Error log for troubleshooting.
-    except Exception:
-        traceback.print_exc(file=sys.stdout)
-    sys.exit(0)
+
--- UI.py
+++ UI.py
@@ -9,6 +9,7 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.setWindowTitle('Email Generator')
         self.setGeometry(0, 0, 400, 200)
         self.move(400, 600)
 
@@ -34,10 +35,72 @@ class MainWindow(QWidget):
         vbox.addLayout(hbox2)
         vbox.addStretch()
 
+        self.btn.clicked.connect(self.preparation)
+        self.btn.clicked.connect(self.windowclose)
 
         self.setLayout(vbox)
         self.show()
 
+    def preparation(self):
 
-class SecondWindow():
-    pass
+        self.preparation = DataPreparation(xl_rows, data_list)
+        self.preparation.datamixer()
+
+        self.preparation = SecondWindow()
+        self.preparation.initui()
+
+    def windowclose(self):
+        self.close()
+
+
+class SecondWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Email Generator')
+        self.setGeometry(0, 0, 400, 200)
+        self.move(400, 600)
+
+    def initui(self):
+        self.label = QLabel(start)
+        self.btn1 = QPushButton('Yes')
+        self.btn2 = QPushButton('No')
+
+        hbox = QHBoxLayout()
+        hbox.addStretch()
+        hbox.addWidget(self.label)
+        hbox.addStretch()
+
+        hbox2 = QHBoxLayout()
+        hbox2.addStretch()
+        hbox2.addWidget(self.btn1)
+        hbox2.addWidget(self.btn2)
+        hbox2.addStretch()
+
+        vbox = QVBoxLayout()
+
+        vbox.addStretch()
+        vbox.addLayout(hbox)
+        vbox.addLayout(hbox2)
+        vbox.addStretch()
+
+        self.setLayout(vbox)
+        self.show()
+
+    def windowclose(self):
+        self.close()
+
+
+class ThirdWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Email Generator')
+        self.setGeometry(0, 0, 400, 200)
+        self.move(400, 600)
+
+    def initui(self):
+        self.btn = QPushButton()
+
+
+
+        self.show()
